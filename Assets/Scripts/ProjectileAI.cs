7a40e2cc3c38d46c9d9a95fc0e650a7269e57f8b
Adrian Cieluch <adrian.cieluch@gmail.com>
2014-12-22 12:02:04 +0100

    Projectile

---
+++ Assets/Scripts/ProjectileAI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileAI : MonoBehaviour {
+
+    public GameObject _target;
+    //public GameObject Target { get; set; }
+    public float Speed;
+
+	
+	void Update ()
+    {
+        // Find and move to the target
+        if (_target != null)
+        {
+            Vector3 position = _target.transform.position - this.transform.position;
+            float distance = position.magnitude;
+            Vector3 direction = position / distance;
+
+            this.transform.position += direction * Speed * Time.deltaTime;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
+}
